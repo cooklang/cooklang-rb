a2d98c9b0802aace448556831073aff5a4fd7fa5
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2022-05-13 11:33:57 +0100

    Extract metadata parsing

--- ext/cooklang/cooklang_parser.c
+++ ext/cooklang/cooklang_parser.c
@@ -2,31 +2,40 @@
 
 VALUE cCooklangParser;
 
-
-VALUE parse(VALUE self, VALUE text)
+int parse_metadata(Recipe * recipe, VALUE * metadata)
 {
-	VALUE recipe_klass;
-	VALUE r_recipe;
-    VALUE r_text = StringValue(text);
-    char *recipe_string = RSTRING_PTR(r_text);
-
-    Recipe *recipe = parseRecipeString(recipe_string);
-
-    ListIterator meta_data_iterator;
+	ListIterator meta_data_iterator;
 	Metadata *current_meta_data;
-
-	recipe_klass = rb_const_get_at(rb_mCooklang, rb_intern("Recipe"));
-	r_recipe = rb_class_new_instance(0, NULL, recipe_klass);
-	VALUE metadata = rb_iv_get(r_recipe, "@metadata");
 
 	meta_data_iterator = createIterator(recipe->metaData);
 	current_meta_data = nextElement(&meta_data_iterator);
 
 	while (current_meta_data != NULL) {
-		rb_hash_aset(metadata, rb_str_new2(current_meta_data->identifier), rb_str_new2(current_meta_data->content));
-		// get the next one
+		rb_hash_aset(metadata,
+					 rb_str_new2(current_meta_data->identifier),
+					 rb_str_new2(current_meta_data->content));
 		current_meta_data = nextElement(&meta_data_iterator);
 	}
+
+	// TODO error handling
+	return 0;
+}
+
+VALUE parse(VALUE self, VALUE text)
+{
+	VALUE recipe_klass;
+	VALUE r_recipe;
+	VALUE metadata;
+    VALUE r_text = StringValue(text);
+    char *recipe_string = RSTRING_PTR(r_text);
+
+    Recipe *recipe = parseRecipeString(recipe_string);
+
+	recipe_klass = rb_const_get_at(rb_mCooklang, rb_intern("Recipe"));
+	r_recipe = rb_class_new_instance(0, NULL, recipe_klass);
+	metadata = rb_iv_get(r_recipe, "@metadata");
+
+	parse_metadata(recipe, metadata);
 
     return r_recipe;
 }
